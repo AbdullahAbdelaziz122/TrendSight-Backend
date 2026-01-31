543d534ea1ff42b40286138fdf9f484fdefd0ef9
abdullahabdelaziz122 <abdullah.abdelaziz15@gmail.com>
2026-01-31 18:59:24 +0200

    Convert To Asynch IO operations

--- app/database.py
+++ app/database.py
@@ -1,21 +1,31 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./app.db"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+SQLALCHEMY_DATABASE_URL = "postgresql+asyncpg://abd:password@localhost/TrendSight"
+
+# create_async_engine 
+engine = create_async_engine(
+    SQLALCHEMY_DATABASE_URL,
+    echo=True, # Set to False in production
 )
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# AsyncSession
+AsyncSessionLocal = sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+    autocommit=False,
+    autoflush=False,
+)
 
 Base = declarative_base()
 
 # Dependency for FastAPI routes
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+async def get_db():
+    async with AsyncSessionLocal() as db:
+        try:
+            yield db
+        finally:
+            await db.close()
