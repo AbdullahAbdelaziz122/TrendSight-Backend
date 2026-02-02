9408078d66f0ce76e989029393f1d753f9467afe
abdullahabdelaziz122 <abdullah.abdelaziz15@gmail.com>
2026-02-02 04:56:30 +0200

    Updated local Database connection details

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
-SQLALCHEMY_DATABASE_URL = "postgresql+asyncpg://abd:password@localhost/TrendSight"
+SQLALCHEMY_DATABASE_URL = "postgresql+asyncpg://abd:password@localhost/trendsight"
 
 # create_async_engine 
 engine = create_async_engine(
